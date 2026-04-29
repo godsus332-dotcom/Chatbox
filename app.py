0730298510b97fe7d1f3fad7221f780350b32072
luxcifer <luxcifer@ubantu01.myguest.virtualbox.org>
2026-04-29 06:35:53 +0530

    fix socketio async mode

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode='threading')
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
