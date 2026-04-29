a8625e1cc4f5f2fd2ae895e493a9521b20158fd3
luxcifer <luxcifer@ubantu01.myguest.virtualbox.org>
2026-04-29 06:17:43 +0530

    fix port for render

--- app.py
+++ app.py
@@ -15,5 +15,5 @@ def handle_message(msg):
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT",10000))
     socketio.run(app, host='0.0.0.0', port=port)
